5f0fd27eab3d479eba4060fd1b92c08445fe2e39
graefft <676@holbertonschool.com>
2019-07-22 21:42:13 +0000

    Adds 10

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,11 +1,18 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_depth -
- *
- * Return: 
+ * binary_tree_depth - function that measures the depth of a node
+ * in a binary tree
+ * @tree: pointer to the node to measure the depth
+ * Return: depth of node
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
+	size_t child;
 
+	if (tree == NULL)
+		return (0);
+
+	child = tree->parent ? 1 + binary_tree_depth(tree->parent) : 0;
+	return (child);
 }
