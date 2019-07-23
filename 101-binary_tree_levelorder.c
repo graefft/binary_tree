8c9d2b79344d2b97ca316e97c466b90108ad123a
Farrukh <679@holbertonschool.com>
2019-07-23 22:44:13 +0000

    Add task 101

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -1,11 +1,43 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_levelorder -
- *
- * Return: 
+ * print_level - function that goes through a binary tree
+ * using level-order traversal
+ * @root: binary tree
+ * @level: level of @root
+ * @func: pointer to a function to call for each node
+ * Return: true or false
+ */
+int print_level(binary_tree_t *root, int level, void (*func)(int))
+{
+	int left, right;
+
+	if (root == NULL)
+		return (0);
+
+	if (level == 1)
+	{
+		func(root->n);
+		return (1);
+	}
+	left = print_level(root->left, level - 1, func);
+	right = print_level(root->right, level - 1, func);
+
+	return (left || right);
+}
+
+
+/**
+ * binary_tree_levelorder - function that goes through a binary tree
+ * using level-order traversal
+ * @tree: binary tree
+ * @func: pointer to a function to call for each node
+ * Return: Nothing.
  */
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
+	int level = 1;
 
+	while (print_level((binary_tree_t *)tree, level, func))
+		level++;
 }
