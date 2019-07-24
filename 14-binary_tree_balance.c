3d49c1372a1f3dac445e56d7987130852882f992
graefft <676@holbertonschool.com>
2019-07-24 19:40:52 +0000

    Updates 14 and 16

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -43,6 +43,9 @@ int binary_tree_balance(const binary_tree_t *tree)
 {
 	int left, right;
 
+	if (tree == NULL)
+		return (0);
+
 	left = binary_tree_height(tree->left);
 	right = binary_tree_height(tree->right);
 	return (left - right);
--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -43,6 +43,9 @@ int binary_tree_balance(const binary_tree_t *tree)
 {
 	int left, right;
 
+	if (tree == NULL)
+		return (0);
+
 	left = binary_tree_height(tree->left);
 	right = binary_tree_height(tree->right);
 	return (left - right);
@@ -77,5 +80,8 @@ int binary_tree_is_full(const binary_tree_t *tree)
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
+	if (tree == NULL)
+		return (0);
+
 	return (binary_tree_is_full(tree) == 1 && binary_tree_balance(tree) == 0);
 }
