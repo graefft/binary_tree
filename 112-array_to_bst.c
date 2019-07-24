1fbd814741640c9470ae9b985eca668215df66c7
Farrukh <679@holbertonschool.com>
2019-07-24 22:02:17 +0000

    Add task 112

--- 112-array_to_bst.c
+++ 112-array_to_bst.c
@@ -1,11 +1,22 @@
 #include "binary_trees.h"
 
 /**
- * array_to_bst -
- *
- * Return: 
+ * array_to_bst - function that builds a Binary Search Tree from an array
+ * @array: array to be converted to BST
+ * @size: size of the @array
+ * Return: BST
  */
 bst_t *array_to_bst(int *array, size_t size)
 {
+	size_t i = 0;
+	bst_t *tree = NULL;
 
+	if (array == NULL)
+		return (NULL);
+	while (i < size)
+	{
+		bst_insert(&tree, array[i]);
+		i++;
+	}
+	return (tree);
 }
