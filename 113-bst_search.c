220acc564c5ff7c20c1df47d0f7dc3a608cba953
graefft <676@holbertonschool.com>
2019-07-24 22:18:27 +0000

    Adds 113

--- 113-bst_search.c
+++ 113-bst_search.c
@@ -1,11 +1,20 @@
 #include "binary_trees.h"
 
 /**
- * bst_search -
- *
- * Return: 
+ * bst_search - function that searches for a value in a BST
+ * @tree: pointer to root node of BST to search
+ * @value: value to search for
+ * Return: pointer to node containing value equal to 'value' or NULL
  */
 bst_t *bst_search(const bst_t *tree, int value)
 {
+	if (tree == NULL)
+		return (NULL);
 
+	if (value == tree->n)
+		return ((bst_t *)tree);
+	else if (value < tree->n)
+		return (bst_search(tree->left, value));
+	else
+		return (bst_search(tree->right, value));
 }
